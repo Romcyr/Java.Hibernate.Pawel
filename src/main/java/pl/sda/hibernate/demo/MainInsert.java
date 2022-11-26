b917134ea8e5eea9240f621ba9c1d16de1abed12
RCyro <r.cyron95@gmail.com>
2022-11-26 13:44:02 +0100

    Zapytanie SELECT * FROM Student

---
+++ src/main/java/pl/sda/hibernate/demo/MainInsert.java
@@ -0,0 +1,44 @@
+package pl.sda.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.time.LocalDate;
+
+    public class MainInsert {
+        public static void main(String[] args) {
+
+            try {
+                Session session = HibernateUtil.INSTANCE.getSessionFactory().openSession();
+                Transaction transaction =  session.beginTransaction();
+//            //tworzymy studenta
+//           Student student = new Student();
+//           student.setImie("Roman");
+//           student.setDataUrodzenia(LocalDate.of(1995,12,12));
+//           student.setKierunekNauczania("Informatyka");
+//           student.setIndeks("928372");
+
+                Student student = Student.builder()
+                        .imie("Roman")
+                        .dataUrodzenia(LocalDate.of(1995,12,12))
+                        .indeks("123123")
+                        .kierunekNauczania("WCHUJ")
+                        .build();
+                //SQL:Insert INTO 'student' values (....)
+
+                // zapisujemy studenta
+                session.persist(student);
+
+                //zatwierdzamy transakcje
+                transaction.commit();
+
+                //jeśli sukces (brak błędu), to nie wywoła siecatch
+            }catch(Exception ioe){
+                // jeśli złapiemy błą∂ to wywoała sie catch
+
+            }
+
+        }
+    }
+
+
