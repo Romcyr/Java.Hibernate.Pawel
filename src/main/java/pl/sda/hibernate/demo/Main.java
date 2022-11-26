3dffda149b7fbcd109bfa2e321deda704816fe56
RCyro <r.cyron95@gmail.com>
2022-11-26 12:42:39 +0100

    Dodanie nowej encji do bazy danych

--- src/main/java/pl/sda/hibernate/demo/Main.java
+++ src/main/java/pl/sda/hibernate/demo/Main.java
@@ -1,10 +1,34 @@
 package pl.sda.hibernate.demo;
 
 import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.io.IOException;
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-     Session session = HibernateUtil.INSTANCE.getSessionFactory().openSession();
-     session.close();
+
+        try {Session session = HibernateUtil.INSTANCE.getSessionFactory().openSession();
+           Transaction transaction =  session.beginTransaction();
+            //tworzymy studenta
+           Student student = new Student();
+           student.setImie("Roman");
+           student.setDataUrodzenia(LocalDate.of(1995,12,12));
+           student.setKierunekNauczania("Informatyka");
+           student.setIndeks("928372");
+
+           // zapisujemy studenta
+           session.persist(student);
+
+           //zatwierdzamy transakcje
+            transaction.commit();
+
+            //jeśli sukces (brak błędu), to nie wywoła siecatch
+        }catch(Exception ioe){
+            // jeśli złapiemy błą∂ to wywoała sie catch
+
+        }
+
     }
 }
---
+++ src/main/java/pl/sda/hibernate/demo/TryCatchPrzykład.java
@@ -0,0 +1,25 @@
+//package pl.sda.hibernate.demo;
+//
+//import org.hibernate.Session;
+//
+//import java.io.IOException;
+//
+public class TryCatchPrzykład {
+    public static void main(String[] args) {
+//        Session session = null;
+//        try {
+//            session = HibernateUtil.INSTANCE.getSessionFactory().openSession();
+//            session.close();
+//            //jeśli sukces (brak błędu), to nie wywoła siecatch
+//        }catch(IOException ioe){
+//            // jeśli złapiemy błą∂ to wywoała sie catch
+//
+//        }finally {
+//            // niezaleznie od ścieżki wykonaniua musi wykonać się zawsze!
+//            session.close();
+//
+//        }
+//
+//    }
+}
+}
