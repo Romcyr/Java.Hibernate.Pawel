5aaf14fe52239b564cca1896a534fa41a9718949
RCyro <r.cyron95@gmail.com>
2022-11-26 12:05:12 +0100

    Dodany model student

---
+++ src/main/java/pl/sda/hibernate/demo/Student.java
@@ -0,0 +1,31 @@
+package pl.sda.hibernate.demo;
+
+//Klasa POJO => Plain Old Jaba Object
+// Klasa która posiada
+// - prywatne pola
+// - gettery, settery
+//- pusty konstruktor
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+@Data
+@Entity
+@NoArgsConstructor
+public class Student {
+    @Id // PRIMARY KEY
+    @GeneratedValue (strategy = GenerationType.IDENTITY)// Auto Increment
+    private Long id;
+
+    @Column(nullable = false)
+    private String imie;                                                    // 'imie'                   VARCHAR(255)
+    private LocalDate dataUrodzenia;                                        // 'data_urodzenia'         DATE
+    private String kierunekNauczania;                                       // 'kierunek_nauczania'     VARCHAR(255)
+    private String indeks; // index jest słowem zabronionymn przez mySQL    // 'indeks;                 VARCHAR(255)
+
+   // private int wiek;                                                       // 'wiek'                   INT NOT NULL
+   // private Integer;                                                        // 'wiek'                   INT
+}
